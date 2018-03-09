37079142b718f3df118a3bc8b95e8d694204400e
SirenNinja <exotictimez@gmail.com>
2018-03-09 00:18:59 -0500

    Added support for translation. Example: !translate from:en to:ru <message>

--- src/main/java/me/sirenninja/evilbot/commands/fun/Translator.java
+++ src/main/java/me/sirenninja/evilbot/commands/fun/Translator.java
@@ -69,18 +69,18 @@ public class Translator implements Command {
     }
 
     private String translate(String langFrom, String langTo, String text) throws IOException {
-        System.out.println("target = " + langTo);
-        System.out.println("source = " + langFrom);
         String urlStr = translateLink + "?q=" + URLEncoder.encode(text, "UTF-8") + "&target=" + langTo + "&source=" + langFrom;
         URL url = new URL(urlStr);
+
         StringBuilder response = new StringBuilder();
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestProperty("User-Agent", "Mozilla/5.0");
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
-        while ((inputLine = in.readLine()) != null) {
+
+        while ((inputLine = in.readLine()) != null)
             response.append(inputLine);
-        }
+
         in.close();
 
         System.out.println(response.toString());
